4b057c146be0b4605b67eeedcee17d79f4f9dd8d
Rafael Jáber <rafael.jaber@hotmail.com>
2024-05-23 19:15:41 -0300

    Adicionado sistemas de informação

--- src/components/cards/experiencia.tsx
+++ src/components/cards/experiencia.tsx
@@ -6,6 +6,7 @@ export function Experiencia() {
       <Title title={"Experiências mais importantes"} />
 
       <ul className="list-disc pl-4">
+        <li>Bacharel em Sistemas de Informação</li>
         <li>Desenvolvimento de um software para gestão de massiva</li>
         <li>Gestão de equipes</li>
         <li>Planejamento técnico</li>
